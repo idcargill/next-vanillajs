a8111c5bdb19cb1844d64295e1643c358e95aeea
Ian Cargill <idcargill@gmail.com>
2023-01-24 19:46:52 -0800

    vanilla js

--- src/sections/HomePage/HomeTitle.tsx
+++ src/sections/HomePage/HomeTitle.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Title, centerText } from '../../../styles/appStyles.css';
 
 type titleProps = {
   title: string;
 }
 
+const titleClass = `${Title} ${centerText}`;
+
 const HomeTitle = ({title}:titleProps): JSX.Element => (
-  <h1>{title}</h1>
+  // <h1 className={`${Title} ${centerText}`}>{title}</h1>
+  <h1 className={titleClass}>{title}</h1>
+
 );
 
 export default HomeTitle;
---
+++ styles/appStyles.css.ts
@@ -0,0 +1,16 @@
+import { style } from '@vanilla-extract/css';
+
+
+export const Title = style({
+  color: 'blue',
+})
+
+export const PageContainer = style({
+  margin: 'auto',
+  padding: 10,
+})
+
+export const centerText = style({
+  textAlign: 'center',
+  margin: 'auto',
+})
