8b00329465ec301db6598b633ea4e33558ba2fe1
Ian Cargill <idcargill@gmail.com>
2023-01-28 20:47:50 +0000

    tabula remove script

--- tabulaRasa.sh
+++ tabulaRasa.sh
@@ -16,19 +16,33 @@ read -p '--> ' userInput
 case $userInput in
   y|Y|yes|Yes)
     echo Deleting files
-    # cp -r src src
-    # cp -r styles styles2
 
-    cat src/pages/index.tsx | grep -v -i 'HomePageExamples' > src/pages/index.tsx
+    # reset index.tsx and remove example files
+    cp src/pages/index.tsx indexCopy.tsx
+    cat indexCopy.tsx | grep -v -i 'HomePageExamples' > src/pages/index.tsx
+    rm indexCopy.tsx
     rm -r src/sections/*
     rm -r styles/StyleSheets/*
     rm styles/index.ts
+    
+    # reset style sheets
     touch styles/index.ts
-    echo "// import { style, keyframes } from '@vanilla-extract/css'" >> styles/index.ts
+    touch styles/StyleSheets/PageContainer.css.ts
+    echo "import { style, keyframes } from '@vanilla-extract/css';" >> styles/StyleSheets/PageContainer.css.ts
+    echo "export const PageContainer = style({ border: '5px solid green', height: '300px', width: '300px' });" >> styles/StyleSheets/PageContainer.css.ts
+    echo  "import { PageContainer } from './StyleSheets/PageContainer.css';" >> styles/index.ts
+    echo "export { PageContainer };" >> styles/index.ts 
+    
+    # set .env.local
     mv examples.env.local .env.local
+    
+    # remove tabulaRasa (self)
     rm $0
     echo  'Welcome to the world'
     tree src
+    
+    echo ''
+    echo "run 'yarn dev' to start server"
     ;;
   n|N|no|NO)
     echo everything stays
