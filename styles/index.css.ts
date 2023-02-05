309ddfae24f10e33a691c8b840fd539543db8cd0
Ian Cargill <idcargill@gmail.com>
2023-02-05 18:42:50 +0000

    w

--- styles/index.css.ts
+++ styles/index.css.ts
@@ -3,10 +3,11 @@ import { style, globalStyle } from '@vanilla-extract/css';
 globalStyle('html, body', {
   margin: 0,
   boxSizing: 'border-box',
+  fontSize: 16,
 });
 
 export const PageContainer = style({ 
-  border: '5px solid green', 
-  height: '300px', 
-  width: '300px', 
+  border: '5px solid green',
+  height: '95vh', 
+  margin: 8,
 });
